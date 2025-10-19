38f109a03bdd45b425f5133df5abfb4c50408939
Otavio Santana <otaviopolianasantana@gmail.com>
2025-10-19 18:11:39 +0100

    feat: inject RoomRepository and add null check in example scenario

--- src/test/java/org/soujava/demos/mongodb/StepDefinitions.java
+++ src/test/java/org/soujava/demos/mongodb/StepDefinitions.java
@@ -3,11 +3,18 @@ package org.soujava.demos.mongodb;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import jakarta.inject.Inject;
+import org.assertj.core.api.Assertions;
+import org.soujava.demos.mongodb.document.RoomRepository;
 
 public class StepDefinitions {
 
+    @Inject
+    private RoomRepository repository;
+
     @Given("an example scenario")
     public void anExampleScenario() {
+        Assertions.assertThat(repository).isNotNull();
     }
 
     @When("all step definitions are implemented")
