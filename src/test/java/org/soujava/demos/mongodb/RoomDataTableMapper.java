e6c1535f5bfe6fd0adf7b833b4fcdcd64dd8b6e7
Otavio Santana <otaviopolianasantana@gmail.com>
2025-10-19 19:00:30 +0100

    test: update RoomDataTableMapper to parse numeric room numbers

--- src/test/java/org/soujava/demos/mongodb/RoomDataTableMapper.java
+++ src/test/java/org/soujava/demos/mongodb/RoomDataTableMapper.java
@@ -1,7 +1,10 @@
 package org.soujava.demos.mongodb;
 
 import io.cucumber.java.DataTableType;
+import org.soujava.demos.mongodb.document.CleanStatus;
 import org.soujava.demos.mongodb.document.Room;
+import org.soujava.demos.mongodb.document.RoomStatus;
+import org.soujava.demos.mongodb.document.RoomType;
 
 import java.util.Map;
 
@@ -10,7 +13,7 @@ public class RoomDataTableMapper {
     @DataTableType
     public Room roomEntry(Map<String, String> entry) {
         return Room.builder()
-                .name(entry.get("name"))
+                .number(Integer.parseInt(entry.get("number")))
                 .type(RoomType.valueOf(entry.get("type")))
                 .status(RoomStatus.valueOf(entry.get("status")))
                 .cleanStatus(CleanStatus.valueOf(entry.get("cleanStatus")))
