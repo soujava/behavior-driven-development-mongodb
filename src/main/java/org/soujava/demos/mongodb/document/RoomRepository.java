6abe0651b06006337e5ab6a8b225767e0278576c
Otavio Santana <otaviopolianasantana@gmail.com>
2025-10-20 04:47:08 +0100

    refactor: remove unused query methods and parameters from RoomRepository

--- src/main/java/org/soujava/demos/mongodb/document/RoomRepository.java
+++ src/main/java/org/soujava/demos/mongodb/document/RoomRepository.java
@@ -1,6 +1,5 @@
 package org.soujava.demos.mongodb.document;
 
-import jakarta.data.repository.Param;
 import jakarta.data.repository.Query;
 import jakarta.data.repository.Repository;
 import jakarta.data.repository.Save;
@@ -13,27 +12,10 @@ public interface RoomRepository {
 
     @Query("FROM Room")
     List<Room> findAll();
-    @Query("WHERE type = 'VIP_SUITE' AND status = 'AVAILABLE' AND underMaintenance = false")
-    List<Room> findVipRoomsReadyForGuests();
-
-    @Query(" WHERE type <> 'VIP_SUITE' AND status = 'AVAILABLE' AND cleanStatus = 'CLEAN'")
-    List<Room> findAvailableStandardRooms();
-
-    @Query("WHERE cleanStatus <> 'CLEAN' AND status <> 'OUT_OF_SERVICE'")
-    List<Room> findRoomsNeedingCleaning();
-
-    @Query("WHERE smokingAllowed = true AND status = 'AVAILABLE'")
-    List<Room> findAvailableSmokingRooms();
-
-    @Save
-    void save(List<Room> rooms);
 
     @Save
     Room save(Room room);
     void deleteBy();
-
-    @Query("WHERE type = :type")
-    List<Room> findByType(@Param("type") String type);
 
     Optional<Room> findByNumber(Integer number);
 }
